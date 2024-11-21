04a6139f57237882a2cf77d7d003a548f2120a12
Mansi Bhatnagar <bhatnagarmansi.03@gmail.com>
2024-11-21 19:13:41 +0530

    Added custom user model

--- notes/models.py
+++ notes/models.py
@@ -1,12 +1,48 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field is required")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, blank=True, null=True, unique=True)
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
 class Note(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, blank=True)
     content = models.TextField()
+
+    """ this is a many to one relationship (one user can have multiple notes), 
+    if a user is deleted then all corresponding note of that user will be deleted 
+    """
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notes', blank=True, null=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
